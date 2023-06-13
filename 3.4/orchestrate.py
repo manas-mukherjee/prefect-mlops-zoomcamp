6798ece36b491b4ccf0b5463de1caf6550e3af83
Manas Mukherjee <manas_mukherjee@intuit.com>
2023-06-12 22:39:50 -0700

    added logger. import os. listdir

--- 3.4/orchestrate.py
+++ 3.4/orchestrate.py
@@ -19,6 +19,8 @@ def read_data(filename: str) -> pd.DataFrame:
     logger = get_run_logger()
     logger.info("INFO level log message.")
     logger.info(f"Current_Directory: {os.getcwd()}")
+    logger.info(os.listdir())
+    
     """Read data into DataFrame"""
     df = pd.read_parquet(filename)
 
@@ -117,8 +119,8 @@ def train_best_model(
 
 @flow
 def main_flow(
-    train_path: str = "./prefect-mlops-zoomcamp/data/green_tripdata_2023-01.parquet",
-    val_path: str = "./prefect-mlops-zoomcamp/data/green_tripdata_2023-02.parquet",
+    train_path: str = "./data/green_tripdata_2023-01.parquet",
+    val_path: str = "./data/green_tripdata_2023-02.parquet",
 ) -> None:
     """The main training pipeline"""
 
